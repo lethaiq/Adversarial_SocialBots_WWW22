d3abd0262d16d3f2d3b936497931a40ac44b2882
lethaiq <leqthai.vn@gmail.com>
2021-05-30 15:58:33 -0400

    re-organize

--- ppo_single_large_hiar.py
+++ ppo_single_large_hiar.py
@@ -9,6 +9,7 @@ import torch
 import numpy as np
 
 SEED = 77
+test_seed = 90
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 
@@ -18,27 +19,27 @@ config = {
     "seed":SEED, 
     "probs":0.8, #set -1 to random
     "graph_algorithm":"node2vec", 
-    "WALK_P":1, 
-    "WALK_Q":50, 
-    "model_type":"CONV",
-    "node_embed_dim":6,
-    "num_filters":8,
+    "WALK_P":1, # parameter p of node2vec
+    "WALK_Q":50, # parameter q of node2vec
+    "model_type":"CONV", 
+    "node_embed_dim":6, # node embedding dimension of node2vec
+    "num_filters":8, # number of filters for CONV
     "validation_graphs":[],
-    "reward_shaping":None,
-    "num_workers":5,
-    "num_gpus":1,
-    "graph_feature":"gcn",
-    "lr":0.0003,
-    "entropy_coeff":0.01,
-    "training_iteration":10000,
-    "checkpoint_freq":5,
-    "wandb_key":"5d4247fa5b879af8aeb0874889a94ca78d4be18d"
+    "reward_shaping":None, 
+    "num_workers":5, # number of workers used during train/test
+    "num_gpus":1, # number of GPUS
+    "graph_feature":"gcn", # gcn means node2vec features
+    "lr":0.0003, # learning rate
+    "entropy_coeff":0.01, # ppo parameter
+    "training_iteration":10000, # number of training iterations
+    "checkpoint_freq":5, # frequency of saving checkpoints during training
+    "wandb_key":"" #wandb API (replace with your own)
 }
 
 config_test = {
-    "custom_max_step": 120,
-    "detection_interval":20,
-    "greedy": False,
+    "custom_max_step": 120, # we train on 60 timesteps be default but during test we test on longer 120
+    "detection_interval":20, # interval K refered in the paper
+    "greedy": False, # whether test the AgentI+H in the paper (heuristic method)
 }
 
 if __name__ == '__main__':
@@ -52,6 +53,6 @@ if __name__ == '__main__':
         assert len(test_graphs)
         
         config["validation_graphs"] = test_graphs
-        config["seed"] = 90
+        config["seed"] = test_seed
 
         test(model_path, **config, **config_test)
